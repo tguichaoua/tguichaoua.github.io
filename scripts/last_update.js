61108e4d3f51c9f0aa281e8f3959869d2cbc6cf2
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2022-09-15 11:37:57 +0200

    ✨ last update date

---
+++ scripts/last_update.js
@@ -0,0 +1,15 @@
+async function set_last_update_date() {
+  const el = document.getElementById('last-update');
+  if (el === undefined) {
+    return;
+  }
+  const last_commit = await fetch('https://api.github.com/repos/tguichaoua/tguichaoua.github.io/commits/main').then(
+    (res) => res.json(),
+  );
+  const date = new Date(last_commit.commit.author.date);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+
+  el.innerHTML = date.toLocaleDateString(undefined, options);
+}
+
+window.addEventListener('load', set_last_update_date);
