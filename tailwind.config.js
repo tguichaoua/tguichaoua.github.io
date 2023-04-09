7167ea517f14caa18ac565ae686fb664ad627ee2
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2023-04-09 10:14:10 +0200

    💄 add some fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "cursive"],
+        "rubik-glitch": ['"Rubik Glitch"', "cursive"],
+        "press-start": ['"Press Start 2P"', "cursive"],
+        silkscreen: ["Silkscreen", "cursive"],
+        alkatra: ["Alkatra", "cursive"],
+        "gajraj-one": ['"Gajraj One"', "cursive"],
+      },
+    },
   },
   plugins: [],
 };
