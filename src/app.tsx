888ca849c66df48bae89153bcaf28ae154042ff1
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2023-04-09 12:31:39 +0200

    💬 add footer and social links

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { FaGithubSquare } from "react-icons/fa";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 import { TextSection } from "./components/text-section";
 import { MagicText } from "./components/magic-text";
@@ -27,38 +27,24 @@ export function App() {
             <MagicText color="blue">Rustacean</MagicText>
           </p>
         </div>
+
+        <div className="flex flex-row items-center justify-center gap-3 text-4xl mt-6">
+          <a href="https://github.com/tguichaoua" target="_blank">
+            <FaGithubSquare />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tristan-guichaoua/"
+            target="_blank"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
       </header>
 
       <hr className="w-[30%] mx-auto h-px my-8 bg-gray-700 border-0 dark:bg-gray-200" />
 
       <main className="flex flex-col items-center w-[80%] mx-auto">
-        <TextSection title="About me" className="w-full mb-3">
-          <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            mattis sapien placerat sagittis volutpat. Donec at metus
-            condimentum, blandit mi in, efficitur ante. Morbi cursus convallis
-            metus, eget finibus tellus pharetra id. Curabitur nisl libero,
-            malesuada eu magna non, condimentum euismod ante. Cras interdum
-            volutpat enim. Fusce ac magna eu orci blandit placerat. In sit amet
-            cursus elit. Integer id nulla est. Nam quis aliquet diam, vel
-            laoreet quam. Nullam feugiat felis ut nisl laoreet, at bibendum
-            purus dignissim. Cras sodales ornare aliquam.
-          </Paragraph>
-        </TextSection>
-        <TextSection title="Some Long Text" className="w-full mb-3">
-          <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            mattis sapien placerat sagittis volutpat. Donec at metus
-            condimentum, blandit mi in, efficitur ante. Morbi cursus convallis
-            metus, eget finibus tellus pharetra id. Curabitur nisl libero,
-            malesuada eu magna non, condimentum euismod ante. Cras interdum
-            volutpat enim. Fusce ac magna eu orci blandit placerat. In sit amet
-            cursus elit. Integer id nulla est. Nam quis aliquet diam, vel
-            laoreet quam. Nullam feugiat felis ut nisl laoreet, at bibendum
-            purus dignissim. Cras sodales ornare aliquam.
-          </Paragraph>
-        </TextSection>
-        <TextSection title="Lorem Ipsum" className="w-full mb-3">
+        <TextSection title="About me" className="w-full">
           <Paragraph>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
             mattis sapien placerat sagittis volutpat. Donec at metus
@@ -72,14 +58,44 @@ export function App() {
           </Paragraph>
         </TextSection>
 
-        <p className="mt-20 whitespace-nowrap">Work in progress ...</p>
+        <hr className="w-[10%] mx-auto h-1 my-8 bg-gray-700 border-0 dark:bg-gray-200" />
 
-        <div className="">
-          <a href="https://github.com/tguichaoua">
-            <FaGithubSquare className="text-5xl" />
-          </a>
-        </div>
+        <TextSection title="Some Long Text" className="w-full">
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            mattis sapien placerat sagittis volutpat. Donec at metus
+            condimentum, blandit mi in, efficitur ante. Morbi cursus convallis
+            metus, eget finibus tellus pharetra id. Curabitur nisl libero,
+            malesuada eu magna non, condimentum euismod ante. Cras interdum
+            volutpat enim. Fusce ac magna eu orci blandit placerat. In sit amet
+            cursus elit. Integer id nulla est. Nam quis aliquet diam, vel
+            laoreet quam. Nullam feugiat felis ut nisl laoreet, at bibendum
+            purus dignissim. Cras sodales ornare aliquam.
+          </Paragraph>
+        </TextSection>
+
+        <hr className="w-[10%] mx-auto h-1 my-8 bg-gray-700 border-0 dark:bg-gray-200" />
+
+        <TextSection title="Lorem Ipsum" className="w-full">
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            mattis sapien placerat sagittis volutpat. Donec at metus
+            condimentum, blandit mi in, efficitur ante. Morbi cursus convallis
+            metus, eget finibus tellus pharetra id. Curabitur nisl libero,
+            malesuada eu magna non, condimentum euismod ante. Cras interdum
+            volutpat enim. Fusce ac magna eu orci blandit placerat. In sit amet
+            cursus elit. Integer id nulla est. Nam quis aliquet diam, vel
+            laoreet quam. Nullam feugiat felis ut nisl laoreet, at bibendum
+            purus dignissim. Cras sodales ornare aliquam.
+          </Paragraph>
+        </TextSection>
       </main>
+
+      <hr className="w-[30%] mx-auto h-px my-8 bg-gray-700 border-0 dark:bg-gray-200" />
+
+      <footer className="p-5 text-xs sm:text-sm">
+        © 2023 Tristan Guichaoua
+      </footer>
     </>
   );
 }
