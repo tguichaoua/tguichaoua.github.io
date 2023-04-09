e0ac215d7a7a686b67f94ed38cb5fe5776abffb4
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2023-04-09 15:59:14 +0200

    ✨ add guy at the page bottom

--- src/app.tsx
+++ src/app.tsx
@@ -13,22 +13,19 @@ export function App() {
       <header role="banner" className="text-center my-5">
         <CatRule />
 
-        <h1 className="text-3xl sm:text-5xl lg:text-7xl whitespace-nowrap my-5">
+        <h1 className="text-3xl sm:text-5xl lg:text-7xl my-5">
           <MagicText color="red">Tristan Guichaoua</MagicText>
         </h1>
-        <div className="grid grid-cols-3 gap-y-2 max-w-[600px] mx-auto mt-10 text-base sm:text-xl lg:text-3xl">
-          <p className="text-3xl sm:text-5xl lg:text-7xl">🧙‍♂️</p>
-          <p className="text-3xl sm:text-5xl lg:text-7xl">👨‍💻</p>
-          <p className="text-3xl sm:text-5xl lg:text-7xl">🦀</p>
-          <p>
+        <div className="flex flex-row flex-wrap items-center justify-center gap-6 mx-auto mt-5 text-base sm:text-xl lg:text-3xl">
+          <span>
             <MagicText color="blue">Developer</MagicText>
-          </p>
-          <p>
+          </span>
+          <span>
             <MagicText color="blue">Engineer</MagicText>
-          </p>
-          <p>
+          </span>
+          <span>
             <MagicText color="blue">Rustacean</MagicText>
-          </p>
+          </span>
         </div>
 
         <div className="flex flex-row items-center justify-center gap-3 text-4xl mt-6">
@@ -103,7 +100,26 @@ export function App() {
         </TextSection>
       </main>
 
-      <footer className="px-5 py-2 mt-5 text-xs sm:text-sm border-t-2 border-t-gray-700 dark:border-t-gray-200">
+      <footer className="px-5 py-2 mt-[50px] text-xs sm:text-sm border-t-2 border-t-gray-700 dark:border-t-gray-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 -0.5 8 8"
+          shape-rendering="crispEdges"
+          width={32}
+          height={32}
+          className="absolute translate-y-[calc(-100%-9px)]"
+        >
+          <path stroke="white" d="M2 0h1" />
+          <path stroke="white" d="M3 0h1M4 1h1M4 2h1M1 5h1M3 6h1" />
+          <path stroke="white" d="M4 0h1M2 1h1M4 5h1M4 6h1" />
+          <path stroke="white" d="M5 0h1M3 2h1M4 4h2" />
+          <path stroke="gray" d="M3 1h1" />
+          <path stroke="gray" d="M5 1h1" />
+          <path stroke="white" d="M2 2h1M2 7h1M5 7h1" />
+          <path stroke="white" d="M5 2h1M3 3h1M3 5h1" />
+          <path stroke="white" d="M4 3h1" />
+          <path stroke="white" d="M6 3h1M2 4h2" />
+        </svg>
         © 2023 Tristan Guichaoua
       </footer>
     </>
