004845b6fcfaab7360ca344d86e92c5e9b4b9dc4
tguichaoua <tguichaoua.dev@gmail.com>
2023-09-30 15:01:38 +0200

    ♻️ refactorize skills page

--- src/routes/skills.tsx
+++ src/routes/skills.tsx
@@ -2,73 +2,6 @@ import Masonry from 'react-layout-masonry';
 import { Skill, SkillIcon } from '../components/skill-icons';
 
 export function Skills() {
-  interface SkillSection {
-    title: string;
-    skills: Skill[][];
-  }
-
-  const ICON_SIZE = 50;
-  const SKILLS: SkillSection[] = [
-    {
-      title: 'Systems Programming',
-      skills: [['C', 'Cpp', 'Rust']],
-    },
-    {
-      title: 'Web Frontend',
-      skills: [
-        ['Html', 'Css', 'Javascript', 'Typescript'],
-        ['Vite', 'React', 'Tailwind'],
-      ],
-    },
-    {
-      title: 'Web Backend',
-      skills: [['Nginx', 'NodeJs', 'NestJs']],
-    },
-    {
-      title: 'Database',
-      skills: [['MySql', 'Sqlite', 'MongoDB']],
-    },
-    {
-      title: 'Tools',
-      skills: [['Git', 'Github', 'Docker', 'Postman', 'VsCode']],
-    },
-    {
-      title: 'Game Engine',
-      skills: [['Bevy', 'Unity']],
-    },
-    {
-      title: 'Misc.',
-      skills: [
-        ['Python', 'CSharp', 'Lua'],
-        ['Latex', 'Markdown', 'Regex', 'Arduino'],
-      ],
-    },
-  ];
-
-  const skillSections = SKILLS.map(({ title, skills }, i) => {
-    const skillIcons = skills.map((skills, i) => (
-      <div
-        key={i}
-        className="flex flex-wrap flex-row justify-center gap-3 mb-5 w-full"
-      >
-        {skills.map((skill, i) => (
-          <SkillIcon key={i} skill={skill} width={ICON_SIZE} />
-        ))}
-      </div>
-    ));
-
-    return (
-      <div
-        key={i}
-        className="border shadow-solid rounded-md px-5 pt-3"
-        style={{ flex: '1 auto' }}
-      >
-        <h3 className="text-center text-2xl mb-5 font-bold">{title}</h3>
-        {skillIcons}
-      </div>
-    );
-  });
-
   return (
     <main className="w-[80%] mx-auto">
       <Masonry
@@ -77,8 +10,68 @@ export function Skills() {
         gap={16}
         columnProps={{ className: 'h-fit' }}
       >
-        {skillSections}
+        <SkillSection
+          title="Systems Programming"
+          skills={[['C', 'Cpp', 'Rust']]}
+        />
+        <SkillSection
+          title="Web Frontend"
+          skills={[
+            ['Html', 'Css', 'Javascript', 'Typescript'],
+            ['Vite', 'React', 'Tailwind'],
+          ]}
+        />
+        <SkillSection
+          title="Web Backend"
+          skills={[['Nginx', 'NodeJs', 'NestJs']]}
+        />
+        <SkillSection
+          title="Database"
+          skills={[['MySql', 'Sqlite', 'MongoDB']]}
+        />
+        <SkillSection
+          title="Tools"
+          skills={[['Git', 'Github', 'Docker', 'Postman', 'VsCode']]}
+        />
+        <SkillSection title="Game Engine" skills={[['Bevy', 'Unity']]} />
+        <SkillSection
+          title="Misc."
+          skills={[
+            ['Python', 'CSharp', 'Lua'],
+            ['Latex', 'Markdown', 'Regex', 'Arduino'],
+          ]}
+        />
       </Masonry>
     </main>
+  );
+}
+
+interface SkillSectionProps {
+  title: string;
+  skills: Skill[][];
+}
+
+function SkillSection(props: SkillSectionProps) {
+  const { title, skills } = props;
+
+  const skillIcons = skills.map((skills, i) => (
+    <div
+      key={i}
+      className="flex flex-wrap flex-row justify-center gap-3 mb-5 w-full"
+    >
+      {skills.map((skill, i) => (
+        <SkillIcon key={i} skill={skill} width={50} />
+      ))}
+    </div>
+  ));
+
+  return (
+    <div
+      className="border shadow-solid rounded-md px-5 pt-3"
+      style={{ flex: '1 auto' }}
+    >
+      <h3 className="text-center text-2xl mb-5 font-bold">{title}</h3>
+      {skillIcons}
+    </div>
   );
 }
