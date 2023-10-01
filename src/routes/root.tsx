cb751aae442f8f85f8ff7911bcef03567cb95583
tguichaoua <tguichaoua.dev@gmail.com>
2023-10-01 21:43:49 +0200

    🍱 import tiny guy svg from file

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -6,6 +6,8 @@ import { CatRule } from '../components/cat-rule';
 import { Nav } from '../components/nav';
 import { LokiText } from '../components/loki-text';
 
+import TinyGuy from '../assets/tiny-guy.svg?react';
+
 export function Root() {
   const onTinyGuyDoubleClick = (event: React.MouseEvent) => {
     event.currentTarget.classList.toggle('dance');
@@ -69,25 +71,11 @@ export function Root() {
 
       <footer className="px-5 py-2 mt-[50px] text-xs sm:text-sm border-t-2 border-t-gray-700 dark:border-t-gray-200">
         <div className="absolute translate-y-[calc(-100%-9px)]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 -0.5 8 8"
-            shapeRendering="crispEdges"
+          <TinyGuy
             width={32}
             height={32}
             onDoubleClick={onTinyGuyDoubleClick}
-          >
-            <path stroke="white" d="M2 0h1" />
-            <path stroke="white" d="M3 0h1M4 1h1M4 2h1M1 5h1M3 6h1" />
-            <path stroke="white" d="M4 0h1M2 1h1M4 5h1M4 6h1" />
-            <path stroke="white" d="M5 0h1M3 2h1M4 4h2" />
-            <path stroke="gray" d="M3 1h1" />
-            <path stroke="gray" d="M5 1h1" />
-            <path stroke="white" d="M2 2h1M2 7h1M5 7h1" />
-            <path stroke="white" d="M5 2h1M3 3h1M3 5h1" />
-            <path stroke="white" d="M4 3h1" />
-            <path stroke="white" d="M6 3h1M2 4h2" />
-          </svg>
+          />
         </div>
         © 2023 Tristan Guichaoua
       </footer>
