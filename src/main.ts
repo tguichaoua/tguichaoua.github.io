15360799fe0f6e163e61534d011828f1202e0b56
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2023-11-29 10:57:21 +0100

    ⚡️ reduce build size

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,4 @@
 import './style.css';
-import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
